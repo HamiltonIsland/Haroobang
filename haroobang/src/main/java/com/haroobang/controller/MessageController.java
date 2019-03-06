b52cb9a66ded1b260caadf95f113de4934a2c9cf
goott6-3 <goott6-3@goott6-3-PC>
2019-03-06 16:51:06 +0900

    messageRoom

--- haroobang/src/main/java/com/haroobang/controller/MessageController.java
+++ haroobang/src/main/java/com/haroobang/controller/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.haroobang.service.MessageService;
 import com.haroobang.vo.AccountVO;
@@ -21,9 +22,31 @@ public class MessageController {
 	
 	//messageRoom창 보여주기
 	@RequestMapping (value = "/messageRoom.action", method = RequestMethod.GET)
-	public String loginView(AccountVO vo, HttpSession session) {	
+	public String messageRoomView(HttpSession session) {
+		if(session.getAttribute("login")==null)
+		{
+			return "redirect:/account/login.action";
+		}
 		
 		return "message/messageRoom";
 	}
+	
+	//messageRoom창 보여주기
+	@RequestMapping (value = "/writeMessage.action", method = RequestMethod.POST)
+	@ResponseBody
+	public String writeMessage(HttpSession session) {			
+		
+		return "messageLoad.action";
+	}	
+	
+	//messageload하기
+	@RequestMapping (value = "/messageLoad.action", method = RequestMethod.GET)
+	public String messageLoad(HttpSession session) {
+		
+		
+		return "message/messageRoomBox";
+	}
+	
+	
 	
 }
