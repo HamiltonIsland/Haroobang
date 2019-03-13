ae04081a6b6bdafa5c95e9eea80af8088e9d7511
goott6-22 <goott6-22@goott6-22-PC>
2019-03-13 17:07:48 +0900

    지희- 현재 예약 페이지(mypage) 환불, 페이저 빼고 완료

--- haroobang/src/main/java/com/haroobang/dao/CurrentReservationDaoImpl.java
+++ haroobang/src/main/java/com/haroobang/dao/CurrentReservationDaoImpl.java
@@ -1,15 +1,31 @@
 package com.haroobang.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import com.haroobang.mapper.CurrentReservationMapper;
+import com.haroobang.vo.ReservationVO;
+import com.haroobang.vo.RoomAttachVO;
 
 @Repository("CurrentReservationDao")
 public class CurrentReservationDaoImpl implements CurrentReservationDao{
 	@Autowired
 	@Qualifier("CurrentReservationMapper")
 	private CurrentReservationMapper currentReservationMapper;
+
+	@Override
+	public List<RoomAttachVO> selectRoomAttachByRoomNo(int roomNo) {
+		List<RoomAttachVO> attachs = currentReservationMapper.selectRoomAttachByRoomNo(roomNo);
+		return attachs;
+	}
+
+	@Override
+	public List<ReservationVO> selectAllCurrentReservations(int memberNo) {
+		List<ReservationVO> currents = currentReservationMapper.selectAllCurrentReservations(memberNo);
+		return currents;
+	}
 
 }
